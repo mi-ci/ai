e8b497af6e8c19f528b1c1bd2a35f9cd61373bbd
mi-ci <stormeight@naver.com>
2024-03-12 17:36:15 +0900

    240312

---
+++ 7장/7-2퍼셉트론으로MNST인식하기SGD옵티마이저282.py
@@ -0,0 +1,24 @@
+import numpy as np
+import tensorflow as tf
+import tensorflow.keras.datasets as ds
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import SGD
+
+(x_train, y_train), (x_test,y_test) = ds.mnist.load_data()
+x_train = x_train.reshape(60000,784)
+x_test = x_test.reshape(10000, 784)
+x_train = x_train.astype(np.float32)/255
+x_test = x_test.astype(np.float32)/255
+y_train = tf.keras.utils.to_categorical(y_train, 10)
+y_test = tf.keras.utils.to_categorical(y_test, 10)
+
+mlp = Sequential()
+mlp.add(Dense(units = 512, activation='tanh', input_shape=(784,)))
+mlp.add(Dense(units = 10, activation='softmax'))
+
+mlp.compile(loss='MSE', optimizer=SGD(learning_rate = 0.1), metrics=['accuracy'])
+mlp.fit(x_train, y_train, batch_size=128, epochs=50, validation_data=(x_test, y_test),verbose=2)
+res = mlp.evaluate(x_test, y_test, verbose=0)
+print('정확률', res[1]*100)
